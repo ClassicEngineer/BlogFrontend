6c621f090315318f464578d05a2216e09daddc20
Classic Engineer <classicdev@yandex.ru>
2023-12-01 22:46:49 +0300

    [dev]: some work on Search.jsx

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ Resources:
 - [StartWithReact](https://www.youtube.com/watch?v=GNrdg3PzpJQ)
 - [FileUpload](https://www.codemzy.com/blog/react-drag-drop-file-upload)
 - [MarkdownEditor](https://github.com/uiwjs/react-md-editor)
+- [DarkMode](https://blog.logrocket.com/dark-mode-react-in-depth-guide/)
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import Header from "../components/Header/Header";
+import classes from "./Pages.module.css";
+import IconButton from "../components/UI/IconButton/IconButton";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import PostPreviewList from "../components/Posts/PostPreviewList";
 
 const Search = () => {
     return (
         <div className="App">
             <h2>Search</h2>
+            <div className={classes.bar}>
+                <input className={classes.searchBar} type="text" title="Search"/>
+                <IconButton className = {classes.searchIcon} icon={faMagnifyingGlass}/>
+                <PostPreviewList posts={[]}/>
+            </div>
         </div>
     );
 };
